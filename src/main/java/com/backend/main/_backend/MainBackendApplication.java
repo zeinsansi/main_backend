1757d7d1d62ad6a7628c03f5f635e51de8c91c17
Zein <zeinkasar123@gmail.com>
2022-10-10 22:24:10 +0200

    Initial commit

---
+++ src/main/java/com/backend/main/_backend/MainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.main._backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/main/_backend/MainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.main._backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
