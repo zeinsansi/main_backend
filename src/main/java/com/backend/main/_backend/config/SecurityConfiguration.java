ff1f901e23dfad8dc1e57d77090ed564519de262
Zein <zeinkasar123@gmail.com>
2023-01-02 17:59:28 +0100

    security configratie gemaakt

---
+++ src/main/java/com/backend/main/_backend/config/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.backend.main._backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SimpleSavedRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .antMatchers("/**/*.{js,html,css}").permitAll()
+                        .antMatchers("/", "/api/user").permitAll()
+                        .antMatchers("/", "/image/home").permitAll()
+                        .antMatchers("/", "/image/fileSystem/{postId}").permitAll()
+                        .antMatchers("/", "/image/profile/{username}").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+//                .csrf((csrf) -> csrf
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                )
+                .csrf().disable()
+                .oauth2Login();
+        return http.build();
+    }
+
+    @Bean
+    public RequestCache refererRequestCache() {
+        return new HttpSessionRequestCache() {
+            @Override
+            public void saveRequest(HttpServletRequest request, HttpServletResponse response) {
+                String referrer = request.getHeader("referer");
+                if (referrer != null) {
+                    request.getSession().setAttribute("SPRING_SECURITY_SAVED_REQUEST",
+                            new SimpleSavedRequest(referrer));
+                }
+            }
+        };
+    }
+}
